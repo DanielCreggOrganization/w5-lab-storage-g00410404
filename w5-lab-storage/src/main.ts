e76701b7ee9c6317ce15ca07f2421779805589f4
g00410404 <145585794+g00410404@users.noreply.github.com>
2024-10-18 09:03:58 +0000

    Home.page.ts updated with getter and setter methods

---
+++ w5-lab-storage/src/main.ts
@@ -0,0 +1,19 @@
+// src/main.ts
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+
+import { Storage } from '@ionic/storage-angular';
+
+// Initialise application with ionic storage
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    Storage,
+  ],
+});
